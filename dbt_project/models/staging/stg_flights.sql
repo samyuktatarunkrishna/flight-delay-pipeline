6f8f13dd705be4c2b477b20b33fd99696815b918
samyuktatarunkrishna <samyuktatarunkrishna@gmail.com>
2026-07-15 22:24:50 +0200

    feat(dbt): create staging views and analytic marts for routing and delays

---
+++ dbt_project/models/staging/stg_flights.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='view') }}
+
+select
+    cast(flight_date as date) as flight_date,
+    cast(carrier as varchar) as carrier_code,
+    cast(flight_num as varchar) as flight_number,
+    cast(origin as varchar) as origin_airport,
+    cast(origin_city as varchar) as origin_city,
+    cast(dest as varchar) as dest_airport,
+    cast(dest_city as varchar) as dest_city,
+    cast(dep_time as varchar) as scheduled_dep_time,
+    cast(dep_delay as integer) as dep_delay_mins,
+    cast(arr_time as varchar) as scheduled_arr_time,
+    cast(arr_delay as integer) as arr_delay_mins,
+    case when cancelled = 1 then true else false end as is_cancelled,
+    case when diverted = 1 then true else false end as is_diverted,
+    cast(carrier_delay as integer) as carrier_delay_mins,
+    cast(weather_delay as integer) as weather_delay_mins,
+    cast(nas_delay as integer) as nas_delay_mins,
+    cast(security_delay as integer) as security_delay_mins,
+    cast(late_aircraft_delay as integer) as late_aircraft_delay_mins,
+    cast(dep_timestamp as timestamp) as actual_dep_timestamp,
+    cast(arr_timestamp as timestamp) as actual_arr_timestamp
+from raw_flights
